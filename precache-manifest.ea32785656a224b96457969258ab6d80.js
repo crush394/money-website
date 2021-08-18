72d015dffb6bd3b29008c37e2c7d3ad81ad028f7
crush <crush_mar@163.com>
2021-08-18 17:41:08 +0800

    deploy

---
+++ precache-manifest.ea32785656a224b96457969258ab6d80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17a3986384cccb9731ae",
+    "url": "/money-website/css/app.d3cf44da.css"
+  },
+  {
+    "revision": "1a9b44de1a5606d79a83029e8fcd4593",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "17a3986384cccb9731ae",
+    "url": "/money-website/js/app.ca4a259c.js"
+  },
+  {
+    "revision": "7e4e75457869a1b7f308",
+    "url": "/money-website/js/chunk-vendors.4d005b3e.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "0b8b1487f1b7477d2e0e0f9c9ffed17a",
+    "url": "/money-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
